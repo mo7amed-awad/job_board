fc6121c55ffc45708a143e22b25c76cba0c7d7fe
mohamed awad <ma7amedawad@gmail.com>
2024-12-05 16:25:13 +0200

    pagination

--- job/views.py
+++ job/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def job_list(request):
     job_list = Job.objects.all()
-    return render(request,'job/job_list.html',{'jobs':job_list})
+
+    paginator = Paginator(job_list,2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,'job/job_list.html',{'jobs':page_obj})
 
 def job_detail(request,id):
     job_detail = Job.objects.get(id=id)
