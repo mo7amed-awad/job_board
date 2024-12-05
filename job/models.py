26d6edaebe767e6c046e2fc34820db04ab438f6e
mohamed awad <ma7amedawad@gmail.com>
2024-12-05 14:15:59 +0200

    add image

--- job/models.py
+++ job/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 '''
@@ -13,6 +13,12 @@ JOB_TYPE=(
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+
+def image_upload(instance, filename):
+    # Safely split the filename and extension
+    imagename, extension = os.path.splitext(filename)
+    return f"jobs/{instance.id}{extension}"
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     job_title = models.CharField(max_length=15,choices=JOB_TYPE)#small amount
@@ -22,6 +28,14 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
+
+    # def save(self, *args, **kwargs):
+    #     # Save the instance first to generate an ID if it doesn't exist
+    #     if not self.id:
+    #         super().save(*args, **kwargs)
+    #     # Re-save to ensure the image uses the correct path
+    #     return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
